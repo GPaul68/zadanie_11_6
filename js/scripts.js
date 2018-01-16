85b484d4ee62ba3896db57c4acbb650e1e19a7d8
Paweł Galeja <pgaleja@gmail.com>
2018-01-16 19:14:52 +0100

    Add function createCard in js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -49,5 +49,22 @@ $ (function() {
             this.$element.remove();
         }
     };
+    //Function contructing class Card
+    function Card(description) {
+        var self = this;
 
+        this.id = randomString();
+        this.description = description;
+        this.$element = createCard();
+
+        function createCard() {
+            var $card = $('<li>').addClass('card');
+            var $cardDescription = $('<p>').addClass('card-description').text(self.description);
+            var $cardDelete = $('<button>').addClass('btn-delete').text('x');
+
+            $cardDelete.click(function() {
+                self.removeCard();
+            });
+        }
+    }
 });
